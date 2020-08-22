e2544ac910c20c1994ca0be5289a8590c448b6f2
OroArmor <eliorona@live.com>
2020-08-21 21:01:13 -0700

    Third commit

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express')
-const app = express()
-const port = 3000
+const path = require('path')
+const app = express().use(express.static(path.join(__dirname, 'public'))
+const port = process.env.PORT || 5000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Example app listening at ${port}`)
 })
