51fad0946a4b7d5927776150bb6ecddf570cc06c
OroArmor <eliorona@live.com>
2020-08-21 21:04:27 -0700

    Fourth

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const path = require('path')
-const app = express().use(express.static(path.join(__dirname, 'public'))
+const app = express().use(express.static(path.join(__dirname, 'public')))
 const port = process.env.PORT || 5000
 
 app.get('/', (req, res) => {
