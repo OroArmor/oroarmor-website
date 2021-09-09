439ae0812631fc992667d23074035fddc1f41453
Eli Orona <eliorona@live.com>
2021-09-09 15:17:04 -0700

    update and serve SSL file

--- server.js
+++ server.js
@@ -20,55 +20,62 @@ const app = new Koa()
 
 // Initializes and attaches Pug
 let pug = new Pug({
-  viewPath: path.resolve(__dirname, 'views', 'pages'),
-  basedir: path.resolve(__dirname, 'views', 'partials'),
-  debug: false,
-  pretty: false,
-  locals: {
-    socialMedia: require('./configs/social.js'),
-    nconf: {
-      domain: process.env.NODE_ENV =='production'? `${process.env.DOMAIN}`:`${process.env.DOMAIN}:${port}`
+    viewPath: path.resolve(__dirname, 'views', 'pages'),
+    basedir: path.resolve(__dirname, 'views', 'partials'),
+    debug: false,
+    pretty: false,
+    locals: {
+        socialMedia: require('./configs/social.js'),
+        nconf: {
+            domain: process.env.NODE_ENV == 'production' ? `${process.env.DOMAIN}` : `${process.env.DOMAIN}:${port}`
+        }
     }
-  }
 })
 pug.use(app)
+
+console.log(process.env.DOMAIN)
+
+router.get("/.well-known/pki-validation/fileauth.txt", (ctx, next) => {
+    ctx.response.body = process.env.SSL_ID;
+})
 
 // Middleware
 app.use(async (ctx, next) => {
     try {
-      await next()
+        await next()
 
-      // Get the status of any responses or assume the request wasn't handled
-      ctx.status = ctx.status || 404
+        // Get the status of any responses or assume the request wasn't handled
+        console.log(router)
+        ctx.status = ctx.status || 404
 
-      // Throw any error codes, or just report and continue
-      if (ctx.status >= 400) ctx.throw(ctx.status)
-      else console.log(`\t--> ${ctx.status} OK`)
+        // Throw any error codes, or just report and continue
+        if (ctx.status >= 400) ctx.throw(ctx.status)
+        else console.log(`\t--> ${ctx.status} OK`)
     } catch (err) {
-      err.status = err.status || 500 // Make sure we have a status code
+        err.status = err.status || 500 // Make sure we have a status code
 
-      // Render the error page
-      ctx.render('error', {
-        error: err,
-        seo: {
-          title: 'Error | OroArmor'
-        }
-      })
-      ctx.status = err.status // Correct the response back to an error response (since ctx.render changes it to 200)
+        // Render the error page
+        ctx.render('error', {
+            error: err,
+            seo: {
+                title: 'Error | OroArmor'
+            }
+        })
+        ctx.status = err.status // Correct the response back to an error response (since ctx.render changes it to 200)
 
-      // Tell Koa that we've handled an error
-      ctx.app.emit('err', err, ctx)
+        // Tell Koa that we've handled an error
+        ctx.app.emit('err', err, ctx)
 
-      // Log the error and our response
-      console.log(err)
-      console.log(`\t--> ${ctx.status} NOT OK: ${err.message}`)
+        // Log the error and our response
+        console.log(err)
+        console.log(`\t--> ${ctx.status} NOT OK: ${err.message}`)
     }
-  })
-  .use(router.routes())
-  .use(router.allowedMethods())
-  .use(serve(__dirname + '/public'))
+})
+    .use(router.routes())
+    .use(router.allowedMethods())
+    .use(serve(__dirname + '/public'))
 
 // Start the server
 module.exports = app.listen(port, () => {
-  console.log(`Server listening on port ${port}`)
+    console.log(`Server listening on port ${port}`)
 })
